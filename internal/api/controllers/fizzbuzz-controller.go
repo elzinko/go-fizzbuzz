70a5389fd1b7938d766a1cf92f61d2a0dc5c4df7
Thomas Couderc <thomas.couderc@gmail.com>
2021-09-02 11:34:37 +0000

    feat: add fizzbuzz controller implementation

--- internal/api/controllers/fizzbuzz-controller.go
+++ internal/api/controllers/fizzbuzz-controller.go
@@ -2,10 +2,58 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/elzinko/go-fizzbuzz/internal/api/services"
 	"github.com/gin-gonic/gin"
 )
 
 func GetFizzFuzz(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "Not yet implemented")
+	int1Str := c.Query("int1")
+	int2Str := c.Query("int2")
+	limitStr := c.Query("limit")
+	str1 := c.Query("str1")
+	str2 := c.Query("str2")
+
+	if int1Str == "" {
+		c.JSON(http.StatusPreconditionFailed, "int1 shouldn't be empty")
+		return
+	}
+	if int2Str == "" {
+		c.JSON(http.StatusPreconditionFailed, "int2 shouldn't be empty")
+		return
+	}
+	if limitStr == "" {
+		c.JSON(http.StatusPreconditionFailed, "limit shouldn't be empty")
+		return
+	}
+	if str1 == "" {
+		c.JSON(http.StatusPreconditionFailed, "str1 shouldn't be empty")
+		return
+	}
+	if str2 == "" {
+		c.JSON(http.StatusPreconditionFailed, "str2 shouldn't be empty")
+		return
+	}
+
+	int1, err := strconv.Atoi(int1Str)
+	if err != nil {
+		c.JSON(http.StatusPreconditionFailed, "int1 should be set to a positive integer")
+		return
+	}
+
+	int2, err := strconv.Atoi(int2Str)
+	if err != nil {
+		c.JSON(http.StatusPreconditionFailed, "int2 should be set to a positive integer")
+		return
+	}
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		c.JSON(http.StatusPreconditionFailed, "limit should be set to a positive integer")
+		return
+	}
+
+	result := services.FizzBuzz(int1, int2, limit, str1, str2)
+
+	c.JSON(http.StatusOK, result)
 }
--- test/fizzbuzz-controller_test.go
+++ test/fizzbuzz-controller_test.go
@@ -13,59 +13,57 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-const notYetImplementedString string = "Not yet implemented"
-
 func Test_FizzBuzz_Controller_OK(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "5", "15", "Fizz", "Buzz")
+	want := "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz"
+	got := FizzBuzz(t, http.StatusOK, "3", "5", "15", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_int1_empty(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "", "5", "15", "Fizz", "Buzz")
+	want := "int1 shouldn't be empty"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "", "5", "15", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_int1_should_be_integer(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "shouldnt_be_a_string", "5", "15", "Fizz", "Buzz")
+	want := "int1 should be set to a positive integer"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "shouldnt_be_a_string", "5", "15", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_int2_empty(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "", "15", "Fizz", "Buzz")
+	want := "int2 shouldn't be empty"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "3", "", "15", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_int2_should_be_integer(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "shouldnt_be_a_string", "15", "Fizz", "Buzz")
+	want := "int2 should be set to a positive integer"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "3", "shouldnt_be_a_string", "15", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_limit_empty(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "5", "", "Fizz", "Buzz")
+	want := "limit shouldn't be empty"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "3", "5", "", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_limit_should_be_integer(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "5", "shouldnt_be_a_string", "Fizz", "Buzz")
+	want := "limit should be set to a positive integer"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "3", "5", "shouldnt_be_a_string", "Fizz", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_str1_empty(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "5", "15", "", "Buzz")
+	want := "str1 shouldn't be empty"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "3", "5", "15", "", "Buzz")
 	check(t, want, got)
 }
 
 func Test_FizzBuzz_Controller_KO_str2_empty(t *testing.T) {
-	want := notYetImplementedString
-	got := FizzBuzz(t, http.StatusNotImplemented, "3", "5", "15", "Fizz", "")
+	want := "str2 shouldn't be empty"
+	got := FizzBuzz(t, http.StatusPreconditionFailed, "3", "5", "15", "Fizz", "")
 	check(t, want, got)
 }
 

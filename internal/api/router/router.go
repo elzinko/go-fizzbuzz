9c2e1cd0cb88dfd533ed26a5b0271396e3d54fdb
Thomas Couderc <thomas.couderc@gmail.com>
2021-09-02 12:23:10 +0000

    feat: add logging to file with nice format

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -1,12 +1,38 @@
 package router
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/elzinko/go-fizzbuzz/internal/api/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func Setup() *gin.Engine {
 	app := gin.New()
+
+	// Logging to a file.
+	f, _ := os.Create("log/api.log")
+	gin.DisableConsoleColor()
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// Logging format
+	app.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("%s - - [%s] \"%s %s %s %d %s \" \" %s\" \" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format("24/Jan/1980:5:00:00 -0100"),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	app.Use(gin.Recovery())
+
 	app.GET("/api/fizzbuzz", controllers.GetFizzFuzz)
 	return app
 }

a187aba2e07b1df0c670bec51359f4874463fdcc
Thomas Couderc <thomas.couderc@gmail.com>
2021-09-02 15:29:13 +0000

    fix: log folder needs initialization

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-	"fmt"
 	"io"
 	"os"
 
@@ -21,15 +20,14 @@ func Setup() *gin.Engine {
 }
 
 func initializeLogging(logFile string) {
-
-	var file, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.Create("log/api.log")
+	// var file, err = os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println("Could Not Open Log File : " + err.Error())
+		log.Errorf("Could Not Open Log File : " + err.Error())
 	}
 
 	mw := io.MultiWriter(os.Stdout, file)
-	log.SetOutput(mw)
 
-	//log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(mw)
 	log.SetFormatter(&log.JSONFormatter{})
 }
